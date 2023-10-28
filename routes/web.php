193f901f4041d7192f32150d2529ce435e882bbf
rodrigoaustincascao <rodrigoaustincascao@gmail.com>
2023-10-28 23:58:10 +0000

    Socialite login github

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/auth/redirect', [LoginController::class, 'redirect']);
+ 
+// Route::get('/auth/callback', [LoginController::class, 'callback']);
+
+Route::get('/auth/redirect', function(){
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function(){
+    $gitHubUser = Socialite::driver('github')->user();
+    $user = User::updateOrCreate(
+        [
+            'github_id' => $gitHubUser->id,
+        ],
+        [
+            'name' => $gitHubUser->name,
+            'email' => $gitHubUser->email,
+            'github_token' => $gitHubUser->token,
+            'github_refresh_token' => $gitHubUser->refreshToken,
+        ]
+    );
+
+    Auth::login($user);
+    return redirect('/dashboard');
+
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
